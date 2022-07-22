9da755c809e05f0549082795fec3ee95f44a04c2
DialloNoussy <abdourahmane.noussydiallo@gmail.com>
2022-07-22 13:59:55 +0200

    resolu

---
+++ script.js
@@ -0,0 +1,21 @@
+// All credits accorded to https://github.com/wesbos & big thanks !
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return
+    e.target.classList.remove('playing')
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.Code}"]`)
+    const key = document.querySelector(`div[data-key="${e.Code}"]`)
+    if (!audio) return
+
+    key.classList.add('playing')
+    audio.currentTime = 0
+    audio.play()
+}
+
+const keys = Array.from(document.querySelectorAll('.key'))
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound)
+
+
